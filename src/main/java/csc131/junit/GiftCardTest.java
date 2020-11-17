770c5a7e960d0981ccdd95ea8805bf51064d651b
Sunya <Sunya@localhost>
2020-11-16 17:09:33 -0800

    Added two methods: getBalance and deduct.

--- src/main/java/csc131/junit/GiftCardTest.java
+++ src/main/java/csc131/junit/GiftCardTest.java
@@ -6,17 +6,16 @@ import org.junit.rules.ExpectedException;
 
 public class GiftCardTest {
 	@Test
-	public void getIssuingStore()
-    {
-        double balance;
-        GiftCard card;
-        int issuingStore;
-        issuingStore = 1337;
-        balance = 100.00;
-        card = new GiftCard(issuingStore, balance);
-        assertEquals("getIssuingStore()",issuingStore, card.getIssuingStore());
-    }
-	
+	public void getIssuingStore() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		assertEquals("getIssuingStore()", issuingStore, card.getIssuingStore());
+	}
+
 	@Test
 	public void getBalance() {
 		double balance;
@@ -25,6 +24,20 @@ public class GiftCardTest {
 		issuingStore = 1337;
 		balance = 100.00;
 		card = new GiftCard(issuingStore, balance);
-		assertEquals("getBalance()",balance, card.getBalance(),0.001);
+		assertEquals("getBalance()", balance, card.getBalance(), 0.001);
 	}
+
+	@Test
+	public void deduct() {
+		double balance;
+		GiftCard card;
+		int issuingStore;
+		String s;
+		issuingStore = 1337;
+		balance = 100.00;
+		card = new GiftCard(issuingStore, balance);
+		s = "Remaining Balance: " + String.format("%6.2f", 90.00);
+		assertEquals("deduct(10.00)", s, card.deduct(10.0));
+	}
+
 }
